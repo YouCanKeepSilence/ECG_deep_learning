1132edc7f9b95cbd87a40c16890c227a925c69e4
Silence <youcankeepsilence@gmail.com>
2020-05-20 23:34:44 +0300

    Refactoring after review, Changed russian comments to eng, moved evaluate funtions to test.py file, test refactoring of CNN model, added ML algorithms

--- dataset.py
+++ dataset.py
@@ -7,21 +7,22 @@ import scipy.io as sio
 import torch
 import torch.utils.data as data_utils
 from keras.preprocessing import sequence
-from sklearn.model_selection import train_test_split
 from torchvision.transforms import transforms
 
 
 class Loader:
-    def __init__(self, record_base_path, reference_path, start_value=4000, end_value=6000, median_length=5000,
-                 pad_value=0):
+    def __init__(self, record_base_path, reference_path,
+                 start_value=4000, end_value=6000,
+                 median_length=5000, pad_value=0):
         self.record_base_path = record_base_path
         self.start_value = start_value
         self.end_value = end_value
         self.median_length = median_length
         self.pad_value = pad_value
+        self.padded_len = median_length * 12
         self.reference = pd.read_csv(reference_path)
 
-    def prepare_input(self, flatten=False):
+    def _prepare_input(self, flatten=False):
         record_paths = glob.glob(os.path.join(self.record_base_path, '*.mat'))
         res = []
         for record_path in record_paths:
@@ -29,7 +30,6 @@ class Loader:
             answer = self.reference[self.reference['Recording'] == name].iloc[0]
             mat = sio.loadmat(record_path)
             ecg = np.array(mat['ECG']['data'][0, 0])
-            # mat = np.transpose(mat)
             if self.end_value >= ecg.shape[1] >= self.start_value:
                 if flatten:
                     padded = sequence.pad_sequences(ecg, dtype=np.float32, maxlen=self.median_length, value=self.pad_value,
@@ -43,9 +43,9 @@ class Loader:
         return res
 
     def load_as_df_for_net(self, normalize=True):
-        net_data = self.prepare_input(flatten=False)
+        data = self._prepare_input(flatten=False)
         names_array = ['gender', 'age', 'label', 'ecg']
-        data = pd.DataFrame.from_records(net_data, index=list(range(0, len(net_data))), columns=names_array)
+        data = pd.DataFrame.from_records(data, index=list(range(0, len(data))), columns=names_array)
         if normalize:
             data.dropna(axis=0, inplace=True)
             data.drop(data[data['age'] == -1].index, inplace=True)
@@ -54,6 +54,105 @@ class Loader:
             data.at[data['gender'] == 'Female', 'gender'] = 1
             data['gender'] = data['gender'].astype(np.float32)
         return data
+
+    def load_as_x_y_for_ml(self, normalize=True, *, augmentation_multiplier=0, augmentation_slice_size=2500):
+        data = self._prepare_input(flatten=True)
+        names_array = ['gender', 'age', 'label', *[f'c_{i}' for i in range(self.padded_len)]]
+        data = pd.DataFrame.from_records(data, index=list(range(0, len(data))), columns=names_array)
+        if normalize:
+            data.dropna(axis=0, inplace=True)
+            data.drop(data[data['age'] == -1].index, inplace=True)
+            data['age'] = data['age'].astype(np.float32)
+            data = self._normalize_data(data, 'label')
+
+        check_na_series = data.isnull().sum()
+        if len(check_na_series[check_na_series > 0].index) != 0:
+            raise Exception('There are some NA values in Dataset')
+
+        if augmentation_multiplier > 0:
+            data = self._augmentation(data, augmentation_multiplier, augmentation_slice_size)
+        x = data.drop('label', axis=1).to_numpy(dtype=np.float32)
+        y = data['label'].to_numpy(dtype=np.long) - 1
+        return x, y
+
+    def _augmentation(self, df, multiplier, slice_size):
+        """
+        Make augmentation of df by slicing n = multiplier times with slice window size = slice_size
+        :param df: source dataset
+        :param multiplier: count of repeat
+        :param slice_size: window size
+        :return: Augmented dataset
+        """
+
+        old_batch = self.median_length
+        sensors_count = 12
+        non_sliceable_columns = df.iloc[:, 0:3]
+        sliceable_columns = df.iloc[:, 3:]
+
+        new_data = pd.DataFrame()
+        final_measures_names = ['c_{}'.format(i + 1) for i in range(slice_size * sensors_count)]
+        for i in range(multiplier):
+            low_border = np.random.randint(0, old_batch - slice_size)
+            indexes = []
+            for k in range(sensors_count):
+                indexes.extend([k * old_batch + low_border + j for j in range(slice_size)])
+
+            sliced = sliceable_columns.iloc[:, indexes]
+            new_measure_names = {x: y for x, y in zip(sliced.columns, final_measures_names)}
+            sliced = sliced.rename(columns=new_measure_names)
+            sliced = pd.concat([non_sliceable_columns, sliced], axis=1, sort=False)
+            new_data = pd.concat([new_data, sliced], axis=0, sort=False, ignore_index=True)
+
+        return new_data
+
+    @staticmethod
+    def _normalize_data(df, target_column_name, columns_to_remove=None):
+        """
+        Make data normalization. For numeric features normalize them regarding mean by std.
+        For categorical features uses OneHot encoding or binarization.
+        Also removes useless columns
+        :param df: Source dataset
+        :param target_column_name: target column (which will be predicted)
+        :param columns_to_remove: columns which should be removed
+        :return: Normalized dataset
+        """
+        # Remove useless columns
+        if columns_to_remove:
+            df.drop(columns_to_remove, axis=1, inplace=True)
+
+        numerical_names = [name for name in df.columns if df[name].dtype.name != 'object']
+        categorical_names = [name for name in df.columns if df[name].dtype.name == 'object']
+
+        # We don't want to do anything with target column, so just remove it from all lists
+        if target_column_name in numerical_names:
+            numerical_names.remove(target_column_name)
+
+        if target_column_name in categorical_names:
+            categorical_names.remove(target_column_name)
+
+        # Make vectorization for all categorical features
+        for categorical_name in categorical_names:
+            unique_values = df[categorical_name].unique()
+            if len(unique_values) == 2:
+                # if this make binarization
+                for idx, value in enumerate(unique_values):
+                    df.at[df[categorical_name] == value, categorical_name] = idx
+
+                categorical_names.remove(categorical_name)
+                numerical_names.append(categorical_name)
+            else:
+                # else we should make onehot encoding, but in our dataset no such columns
+                raise Exception(f'Not implemented OneHot Encoding for categorical column {categorical_name}')
+
+        # Numeric columns normalization
+        columns_to_normalize = df.columns.difference([*categorical_names]).to_list()
+        # Optimize normalization, to fit in memory
+        normalize_batch = 2500
+        for i in range(0, len(columns_to_normalize), normalize_batch):
+            sliced = columns_to_normalize[i:i + normalize_batch]
+            df[sliced] = (df[sliced] - df[sliced].mean(axis=0)) / df[sliced].std(axis=0)
+
+        return df
 
 
 class ECGDataset(data_utils.Dataset):
@@ -96,136 +195,4 @@ class ECGDataset(data_utils.Dataset):
             pass
         return non_ecg_tensor, ecg_tensor, label
 
-
-def get_prepared_dataset(file_path, names, target_column, columns_to_delete, *,
-                         augmentation_multiplier=0, augmentation_slice_size=2500):
-    """
-    Возращает готовый к работе датасет
-    :param file_path: Путь до csv с данными
-    :param names: Имена столбцов
-    :param target_column: Целевой столбец (то, что будет предсказываться)
-    :param columns_to_delete: Столбцы, которые нужно удалить
-    :param augmentation_multiplier: Кол-во повторений в аугментации
-    :param augmentation_slice_size: Размер "окна" в аугментации
-    :return: X, y
-    """
-    df = _load_raw_data(file_path, names)
-    df = _normalize_data(df, target_column, columns_to_delete)
-    if augmentation_multiplier > 0:
-        df = _augmentation(df, augmentation_multiplier, augmentation_slice_size)
-    check_na_series = df.isnull().sum()
-    if len(check_na_series[check_na_series > 0].index) != 0:
-        raise Exception('There are some NA values in Dataset')
-    X = df.drop(target_column, axis=1).to_numpy(dtype=np.float32)
-    y = df[target_column].to_numpy(dtype=np.long)
-    y -= 1
-    return X, y
-
-
-def split(x, y, test_size, random_state=42, batch_size=10000):
-    """
-    Просто обертка для разбития на тестовую и тренировочную выборку
-    :param x:
-    :param y:
-    :param test_size: Размер в процентах тестовой выборки
-    :param random_state:
-    :param batch_size: Размер батча
-    :return: Разбитые данные X_train, X_test, y_train, y_test
-    """
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=random_state)
-    train = data_utils.TensorDataset(torch.from_numpy(X_train).unsqueeze(1), torch.from_numpy(y_train))
-    test = data_utils.TensorDataset(torch.from_numpy(X_test).unsqueeze(1), torch.from_numpy(y_test))
-    train_loader = data_utils.DataLoader(train, batch_size=batch_size)
-    test_loader = data_utils.DataLoader(test, batch_size=batch_size)
-    return train_loader, test_loader
-    # return [(torch.from_numpy(X_train), torch.from_numpy(y_train))], [(torch.from_numpy(X_test), torch.from_numpy(y_test))] # train_loader, test_loader
-
-
-def _load_raw_data(file_path, names):
-    """
-    Загружает данные из csv в pd.DataFrame
-    :param file_path: Путь до csv
-    :param names: Имена столбцов
-    :return: pd.DataFrame
-    """
-    df = pd.read_csv(file_path, names=names)
-    return df
-
-
-def _augmentation(df, multiplier, slice_size):
-    """
-    Аугментирует датасет используя рандомный срез заданной длины, выбирает рандомно заданное число раз
-    :param df: Исходный датасет
-    :param multiplier: Кол-во повторений
-    :param slice_size: Размер "окна" среза
-    :return: Аугментированный датасет
-    """
-
-    old_batch = 5000
-    sensors_count = 12
-    non_sliceable_columns = df.iloc[:, 0:3]
-    sliceable_columns = df.iloc[:, 3:]
-
-    new_data = pd.DataFrame()
-    final_measures_names = ['c_{}'.format(i + 1) for i in range(slice_size * sensors_count)]
-    for i in range(multiplier):
-        low_border = np.random.randint(0, old_batch - slice_size)
-        indexes = []
-        for k in range(sensors_count):
-            indexes.extend([k * old_batch + low_border + j for j in range(slice_size)])
-
-        sliced = sliceable_columns.iloc[:, indexes]
-        new_measure_names = {x: y for x, y in zip(sliced.columns, final_measures_names)}
-        sliced = sliced.rename(columns=new_measure_names)
-        sliced = pd.concat([non_sliceable_columns, sliced], axis=1, sort=False)
-        new_data = pd.concat([new_data, sliced], axis=0, sort=False, ignore_index=True)
-
-    return new_data
-
-
-def _normalize_data(df, target_column_name, columns_to_remove):
-    """
-    Нормализует данные. Для числовых признаков нормализует их относительно мат ожидания
-    Для категориальных признаков используется OneHot encoding или бинаризация
-    Удаляет ненужные столбцы
-    :param df: Исходный датасет
-    :param target_column_name: Имя целевого столба (то, что будет предсказываться)
-    :param columns_to_remove: Колонки, которые необходимо удалить
-    :return: Нормализованный датасет
-    """
-    # Удаляем ненужные столбцы
-    df.drop(columns_to_remove, axis=1, inplace=True)
-
-    numerical_names = [name for name in df.columns if df[name].dtype.name != 'object']
-    categorical_names = [name for name in df.columns if df[name].dtype.name == 'object']
-
-    # Не хотим учитывать целевой как численный или категориальный
-    if target_column_name in numerical_names:
-        numerical_names.remove(target_column_name)
-
-    if target_column_name in categorical_names:
-        categorical_names.remove(target_column_name)
-
-    # Векторизация категориальных признаков, за исключением целевого
-    for categorical_name in categorical_names:
-        unique_values = df[categorical_name].unique()
-        if len(unique_values) == 2:
-            # Если так, то проводим бинаризацию
-            for idx, value in enumerate(unique_values):
-                df.at[df[categorical_name] == value, categorical_name] = idx
-
-            categorical_names.remove(categorical_name)
-            numerical_names.append(categorical_name)
-        else:
-            # Иначе onehot encoding, но тут нет необходимости, т.к. из категориальных только пол
-            raise Exception(f'Not implemented OneHot Encoding for categorical column {categorical_name}')
-
-    # Нормировка числовых переменных
-    numerical_data = df[numerical_names]
-    mean = numerical_data.mean(axis=0)
-    std = numerical_data.std(axis=0)
-    numerical_data = (numerical_data - mean) / std
-    df[numerical_names] = numerical_data
-
-    return df
 
--- models.py
+++ models.py
@@ -9,10 +9,10 @@ class MLP(nn.Module):
 
         self.num_classes = num_classes
         self.input_bn = nn.BatchNorm1d(count_of_features)
-        self.fc0 = nn.Linear(count_of_features, 784)  # полносвязный слой
-        self.fc1 = nn.Linear(784, 512)  # полносвязный слой
-        self.fc2 = nn.Linear(512, 256)  # полносвязный слой
-        self.fc3 = nn.Linear(256, num_classes)  # полносвязный слой
+        self.fc0 = nn.Linear(count_of_features, 784)
+        self.fc1 = nn.Linear(784, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, num_classes)
         self.dropout = nn.Dropout(0.5)
 
     def forward(self, non_ecg, ecg):
@@ -26,42 +26,68 @@ class MLP(nn.Module):
         x = self.dropout(x)
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-        # in reason of CrossEntropy here should be softmax activate function instead of ReLU
-        return F.log_softmax(x, dim=1)
+        # we don't need activation here in reason of CrossEntropyLoss usage. It includes LogSoftmax inside
+        return x
 
 
 class CNN(nn.Module):
-    def __init__(self, num_classes, number_of_channels=12):
+    def __init__(self, num_classes, number_of_channels=12, pooling='avg'):
         super(CNN, self).__init__()
+        if pooling == 'avg':
+            self.pooling = nn.AvgPool1d(kernel_size=3, stride=3)
+        else:
+            self.pooling = nn.MaxPool1d(kernel_size=3, stride=3)
         self.num_classes = num_classes
+        self.conv_1_layer = nn.Sequential(
+            nn.Conv1d(12, 24, kernel_size=3),
+            nn.BatchNorm1d(24),
+            nn.ReLU,
+            self.pooling
+        )
+        self.conv_2_layer = nn.Sequential(
+            nn.Conv1d(24, 48, kernel_size=3),
+            nn.BatchNorm1d(48),
+            nn.ReLU,
+            self.pooling
+        )
+        self.conv_2_layer = nn.Sequential(
+            nn.Conv1d(48, 96, kernel_size=3),
+            nn.BatchNorm1d(96),
+            nn.ReLU,
+            self.pooling
+        )
         self.input_bn = nn.BatchNorm1d(number_of_channels)
         self.conv1 = nn.Conv1d(12, 24, kernel_size=3)
+        self.conv_1_bn = nn.BatchNorm1d(24)
         self.conv2 = nn.Conv1d(24, 48, kernel_size=3)
+        self.conv_2_bn = nn.BatchNorm1d(48)
         self.conv3 = nn.Conv1d(48, 96, kernel_size=3)
-        self.pooling = nn.AvgPool1d(kernel_size=3, stride=3)
         self.dropout = nn.Dropout(0.5)
-        self.fc_bn = nn.BatchNorm1d(8736 + 2)
+        self.fc_input_bn = nn.BatchNorm1d(8736 + 2)
         self.fc1 = nn.Linear(8736 + 2, 2048)
+        self.fc1_bn = nn.BatchNorm1d(2048)
         self.fc2 = nn.Linear(2048, 1024)
+        self.fc2_bn = nn.BatchNorm1d(1024)
         self.fc3 = nn.Linear(1024, 256)
+        self.fc3_bn = nn.BatchNorm1d(256)
         self.fc4 = nn.Linear(256, num_classes)
 
     def forward(self, non_ecg, ecg):
         x = ecg
         x = self.input_bn(x)
-        x = F.relu(self.conv1(x))
-        x = self.pooling(x)
-        x = F.relu(self.conv2(x))
-        x = self.pooling(x)
-        x = F.relu(self.conv3(x))
-        x = self.pooling(x)
+        x = self.conv_1_layer(x)
+        x = self.conv_2_layer(x)
+        x = self.conv_3_layer(x)
         x = x.view(x.size(0), -1)
         x = torch.cat((non_ecg, x), dim=1)
-        x = self.fc_bn(x)
+        x = self.fc_input_bn(x)
         x = self.dropout(x)
         x = F.relu(self.fc1(x))
+        x = self.fc1_bn(x)
         x = F.relu(self.fc2(x))
         x = self.dropout(x)
+        x = self.fc2_bn(x)
         x = F.relu(self.fc3(x))
+        x = self.fc3_bn(x)
         x = self.fc4(x)
-        return F.log_softmax(x, dim=1)
+        return x
